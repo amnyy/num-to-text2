baa468342a92446efbb21491b938a256af5165c3
Amany <68297944+amnyy@users.noreply.github.com>
2020-11-14 02:58:55 +0330

    Using library in client activity (with no library source)

--- app/src/main/java/com/demo/numtotext/DemoActivity.kt
+++ app/src/main/java/com/demo/numtotext/DemoActivity.kt
@@ -2,10 +2,25 @@ package com.demo.numtotext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import com.nums.numtext.NumText
 
 class DemoActivity : AppCompatActivity() {
+
+    private val converter = NumText()
+    private lateinit var editor: EditText
+    private lateinit var tv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
+        editor = findViewById(R.id.nEdit)
+        tv = findViewById(R.id.nTv)
+    }
+
+    fun btnClick(v: View){
+        tv.text = converter.getNumInText(editor.text.toString().toIntOrNull() ?: 0)
     }
 }
